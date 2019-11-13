ec7a03ff6cf292303401e70c2377d746cf1557c7
Jordan Safford and Trevor Mackin <jordan@>
2019-11-13 15:02:43 -0800

    Add tests with Jest.

--- src/triangle.js
+++ src/triangle.js
@@ -19,3 +19,34 @@ Triangle.prototype.checkType = function() {
     return "isosceles triangle";
   }
 };
+
+
+export function Rectangle(side1, side2, side3, side4){
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
+  this.side4 = side4;
+}
+
+Rectangle.prototype.checkSides = function() {
+  if ((this.side1 === this.side3) && (this.side2 === this.side4)){
+    return "rectangle";
+  }
+};
+
+
+Rectangle.prototype.isSquare = function() {
+  if ((this.side1 === this.side2) && (this.side2 === this.side3) && (this.side3 === this.side4) && (this.side4 === this.side1)){
+    return "square"
+  }
+}
+
+export function add(num1, num2){
+  var total = num1 + num2;
+  return total;
+}
+
+export function multiply(num1, num2, num3){
+  var total = num1 * num2 * num3;
+  return total;
+}
