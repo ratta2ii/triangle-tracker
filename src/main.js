465643fbbb067c6bd8d90f9af82af92b6f924e14
Trevor <tismetrm@gmail.com>
2019-11-12 23:07:55 -0800

    Add webpack including lint, Jest for testing, jquery, bootstrap, etc... Project template.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
-import { pingPong } from './ping-pong';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Triangle } from './triangle';
+
+
+
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
+
+  $('.triangle-form').submit(function(event){
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+
+    var side1t = $('#side1').val();
+    var side2t = $('#side2').val();
+    var side3t = $('#side3').val();
+
+    var endResult = new Triangle(side1t, side2t, side3t);
+
+    $("#result").text(endResult);
+    $(".result").show();
   });
+
 });
